9b6606d44314cec8cea07052591bb7deb31d5ffd
sanghyun0127 <ash0127@khu.ac.kr>
2021-01-26 17:10:40 +0900

    project set up & add firebase

---
+++ src/service/auth_service.js
@@ -0,0 +1,12 @@
+// firebase 로그인 구현
+
+import firebase from "firebase";
+
+class AuthService {
+  login(providerName) {
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebase.auth().signInWithPopup(authProvider);
+  }
+}
+
+export default AuthService;
---
+++ src/service/firebase.js
@@ -0,0 +1,14 @@
+//firebase SDK
+
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//보안상 이유로 따로 env에 보관
+//gitignore에도 설정해주기
